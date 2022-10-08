5abce44384a6730b796be1d7fce4d02d0201af40
Rodolfo Andrés Rivas Matta <rodolforivasmatta@gmail.com>
2022-10-08 13:52:09 -0400

    Testing the Running_Process class.

--- src/main.cpp
+++ src/main.cpp
@@ -39,5 +39,38 @@ void test_routine() {
     bursts = { 3, 12, 4 };
     processes.push_back(OS_Scheduler_Simulator::Engine::Process_Data("P4", bursts));
 
+    // First breakpoint for debugging. INFO: Testing the Process_Data class.
+    std::list<OS_Scheduler_Simulator::Engine::Process_Data>::iterator it(processes.begin());
+    
+    // Running.
+    OS_Scheduler_Simulator::Engine::Running_Process running(&(* it));
+    running.send_to_cpu();
+
+    // Ready list.
+    it = std::next(it);
+    std::list<OS_Scheduler_Simulator::Engine::Running_Process> ready_list;
+    ready_list.push_back(&(*it));
+
+    it = std::next(it);
+    ready_list.push_back(&(*it));
+
+    it = std::next(it);
+    ready_list.push_back(&(*it));
+
+    // Second breakpoint for debugging. INFO: Testing constructor of Running_Process class.
+
+    // After first burst completed.
+    constexpr unsigned time{ 5 };
+
+    OS_Scheduler_Simulator::Engine::Running_Process temp = running.get_next_process_state(time);
+    for (OS_Scheduler_Simulator::Engine::Running_Process& process : ready_list) process.get_next_process_state(time);
+    
+    ready_list.push_back(temp);
+    
+    running = ready_list.front();
+    running.send_to_cpu();
+    ready_list.pop_front();
+
+    // Third breakpoint for debugging. INFO: Testing one iteration in a FCFS algorithm fashion.
 }
 #endif // _DEBUG
