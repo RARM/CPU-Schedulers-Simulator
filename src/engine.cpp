46e5d65c1102e71604d172af84ae13dc476bc920
Rodolfo Andrés Rivas Matta <rodolforivasmatta@gmail.com>
2022-10-08 11:57:04 -0400

    Completed the Process_Data class.

--- src/engine.cpp
+++ src/engine.cpp
@@ -5,3 +5,8 @@
 #include <vector>
 #include <functional>
 #include <iterator>
+
+OS_Scheduler_Simulator::Engine::Process_Data::Process_Data(std::string name, std::span<unsigned> operations_list) 
+    : name(name), operations(operations_list.size()) {
+    for (unsigned i{ 0 }; i < operations_list.size(); i++) this->operations.at(i) = operations_list[i];
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,43 @@
 #include <iostream>
 #include <list>
 #include <iterator>
+#include <array>
 #include "engine.h"
 
 // SECTION: Algorithms (FCFS, SJF, MLFQ).
 // std::list<OS_Scheduler_Simulator::Engine::Data_Time_Point> algorithm_FCFS(std::list<OS_Scheduler_Simulator::Engine::Process> data);
 
+#ifdef _DEBUG
+void test_routine();
+#endif // _DEBUG
+
+
 int main(void) {
 
 #ifdef _DEBUG
     std::cout << "Running debugging version." << std::endl;
+    test_routine();
 #endif // DEBUG
 
     return 0;
 }
+
+#ifdef _DEBUG
+void test_routine() {
+    // Creating four processes.
+    std::list<OS_Scheduler_Simulator::Engine::Process_Data> processes;
+
+    std::vector<unsigned> bursts = { 5, 8, 3 };
+    processes.push_back(OS_Scheduler_Simulator::Engine::Process_Data("P1", bursts));
+
+    bursts = { 4, 3, 5 };
+    processes.push_back(OS_Scheduler_Simulator::Engine::Process_Data("P2", bursts));
+
+    bursts = { 8, 1, 2 };
+    processes.push_back(OS_Scheduler_Simulator::Engine::Process_Data("P3", bursts));
+
+    bursts = { 3, 12, 4 };
+    processes.push_back(OS_Scheduler_Simulator::Engine::Process_Data("P4", bursts));
+
+}
+#endif // _DEBUG
